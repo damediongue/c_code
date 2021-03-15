a25bb9c94bcf988bd7a846585ce3dac97635407d
damediongue <50827732+damediongue@users.noreply.github.com>
2021-03-15 18:58:34 +0000

    Add files via upload

---
+++ PC2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   
+    /* Déclarer les variables manquantes */
+
+    printf("Entrer un nombre réel: ");
+    multiplie = (int)((n+0.005)*100);
+    arrondi = multiplie /100.0;
+    printf("%f arrondi à deux chiffres donne: %.2f\n", n, arrondi);
+
+    return 0;
+}
---
+++ PC3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    float rayon, perimetre, aire;
+    printf("Entrer le rayon: ");
+    scanf("%f", &rayon);
+
+    /* Partie à compléter  avec des 
+    * expressions mathématiques  ***
+    */
+    
+    aire = PI*rayon*rayon;
+    printf("Périmetre = %f\n Aire = %f\n", perimetre, aire);
+
+    /* Compléter le ou les instruction(s) manquante(s) */
+}
