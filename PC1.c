2c603929802da9b20dbabcbf006f8fdbda6f1cdb
damediongue <50827732+damediongue@users.noreply.github.com>
2021-03-15 18:59:00 +0000

    Add files via upload

---
+++ PC1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   
+    /* Déclarer ici les variables manquantes */
+
+    printf("Enter un nombre :");
+    scanf("%d",&n);
+
+    dizaine = ?????;
+    centaine = ?????;
+    printf("Le chiffre des dizaines est: %d", dizaine);
+    printf("Le chiffre des centaine est: %d", centaine);
+
+    return 0;
+}
